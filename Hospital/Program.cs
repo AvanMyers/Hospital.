dd3d490c62369d3543b605027cfd9f173bdf9304
AvanMyers <smail1341@gmail.com>
2024-07-16 15:49:09 +0300

    Initial commit

---
+++ Hospital/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int piopleInHospital;
+            int timePerPersone = 10;
+            int hoursBeforeAppointment;
+            int minutesBeforAppointment;
+            int minutesInHour = 60;
+
+            Console.WriteLine("Введите количество человек в очереди");
+            piopleInHospital = Convert.ToInt32(Console.ReadLine());
+            hoursBeforeAppointment = (piopleInHospital * timePerPersone) / minutesInHour;
+            minutesBeforAppointment = (piopleInHospital * timePerPersone) % minutesInHour;
+
+            Console.WriteLine($"Вам осталось подождать {hoursBeforeAppointment} часов и {minutesBeforAppointment} минут");
+            Console.ReadKey();
+
+        }
+    }
+}
